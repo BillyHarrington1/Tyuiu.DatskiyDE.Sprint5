022be1b8e8b9e07dc7b16b1222a5111a83cad3bc
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-26 02:55:09 +0500

    Task5

---
+++ Tyuiu.DatskiyDE.Sprint5.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.DatskiyDE.Sprint5.Task5.V4.Lib;
+namespace Tyuiu.DatskiyDE.Sprint5.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            
+        }
+    }
+}

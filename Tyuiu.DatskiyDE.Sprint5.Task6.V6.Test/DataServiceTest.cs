e03c40320d426a85bc7fafef435e8846c52dd499
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-29 04:56:45 +0500

    Task6

---
+++ Tyuiu.DatskiyDE.Sprint5.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.DatskiyDE.Sprint5.Task6.V6.Lib;
+namespace Tyuiu.DatskiyDE.Sprint5.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V6.txt";
+
+            int result = ds.LoadFromDataFile(path);        
+            Assert.AreEqual(22, result); 
+        }
+    }
+}
---
+++ Tyuiu.DatskiyDE.Sprint5.Task6.V6/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.DatskiyDE.Sprint5.Task6.V6.Lib;
+namespace Tyuiu.DatskiyDE.Sprint5.Task6.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Дацкий Д. Е. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Дацкий Денис Евгеньевич. | ИСПб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл C:\\DataSprint5\\InPutDataFileTask6V6.txt                      *");
+            Console.WriteLine("* Найти количество строчных русских букв в заданной строке.               *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V6.txt";
+
+            Console.WriteLine("Файл для чтения: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.LoadFromDataFile(path);
+            Console.WriteLine($"Количество строчных русских букв: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}

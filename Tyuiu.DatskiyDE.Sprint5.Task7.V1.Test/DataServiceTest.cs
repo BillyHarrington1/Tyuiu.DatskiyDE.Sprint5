880641745aa99686fa1ff9cd3bac35b75037c6e8
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-29 05:11:56 +0500

    Task7

---
+++ Tyuiu.DatskiyDE.Sprint5.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.DatskiyDE.Sprint5.Task7.V1.Lib;
+namespace Tyuiu.DatskiyDE.Sprint5.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string testContent = "Привет123";
+            string tempPath = Path.GetTempFileName();
+            File.WriteAllText(tempPath, testContent);
+
+            DataService ds = new DataService();
+            string result = ds.LoadDataAndSave(tempPath);
+
+            File.Delete(tempPath);
+
+            Assert.AreEqual("Привет", result);
+        }
+    }
+}
